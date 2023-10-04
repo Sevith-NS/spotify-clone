045e695d13509eac042c48602b3723b1cca37b07
Sevith <nssevith2020@gmail.com>
2023-10-04 21:33:13 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  images: {
+      domains: [
+          "ozhonmpretqdbiwnkhoe.supabase.co"
+      ]
+  }
+}
 
 module.exports = nextConfig
